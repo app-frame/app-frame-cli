15628e38ae62e94162373ccc71891468f01c4932
Adipat Larprattanakul <adipat.larprattanakul@thomsonreuters.com>
2016-02-18 10:48:21 +0700

    download sample from github

--- bin/common.js
+++ bin/common.js
@@ -1,5 +1,24 @@
-function createElectronApp() {
-    console.log('Hi electron App');
+
+var Download = require('download');
+ 
+function createElectronApp() {    
+    console.log('cloning electron sample...')
+    new Download(
+        {
+            mode: '755',
+            extract: true,
+            strip: 1
+        })
+        .get('https://github.com/app-frame/app-frame-electron-template/archive/master.zip')
+        .dest('.')
+        .run(function(err, files) {
+            if (err) {
+                console.log('error on downloading sample from github.');
+                console.log('Please check your connection to https://github.com/app-frame/app-frame-electron-template');
+            }
+            else
+                console.log('download successful');
+        });
 }
 
 module.exports.createElectronApp = createElectronApp;
